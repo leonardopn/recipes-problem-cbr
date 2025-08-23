c5549ad9a3b0ec72944e164d03069ec1b30ca9e2
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2025-08-23 18:16:42 -0300

    feat: :sparkles: Implement data loading function and update project dependencies

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import pandas as pd
+
+
+DATASET_FILE = "./datasets/recipes.csv"
+
+
+# 1. Carregar o dataset
+# Certifique-se de que o nome do arquivo corresponde ao que você baixou.
+# Se o arquivo estiver em outra pasta, coloque o caminho completo.
+def load_data_set() -> pd.DataFrame:
+    try:
+        df = pd.read_csv(DATASET_FILE)
+        print(df.head())
+        return df
+    except FileNotFoundError:
+        print(
+            "Erro: Arquivo CSV não encontrado. Verifique o nome e o caminho do arquivo."
+        )
+        exit()
+
+
+# ===================================== APP ===================================== #
+
+
 def main() -> None:
-    print("Hello from recipes-problem-cbr!")
+    load_data_set()
 
 
 if __name__ == "__main__":
