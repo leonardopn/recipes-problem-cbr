9f8e1610ae90d513a482079ddb654a581cc7b618
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2025-08-23 18:30:23 -0300

    feat: add asyncio support and enhance dataset loading
    - Added asyncio import to enable asynchronous operations.
    - Updated main.py to include print statements for process start and end.
    - Modified pyproject.toml to include watchfiles as a development dependency.
    - Updated uv.lock to reflect the addition of anyio and watchfiles packages.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import asyncio
+from watchfiles import awatch, run_process
 
 
 DATASET_FILE = "./datasets/recipes.csv"
@@ -23,8 +25,12 @@ def load_data_set() -> pd.DataFrame:
 
 
 def main() -> None:
+    print(f"{'=' * 80}Iniciando o processamento...{'=' * 80}\n")
+
     load_data_set()
+
+    print(f"\n{'=' * 80} Fim do processamento {'=' * 80}\n")
 
 
 if __name__ == "__main__":
-    main()
+    run_process("./", target=main)
